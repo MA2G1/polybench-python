2d3e74fd9620e109d773c756c6e115a320a01863
Miguel Ángel Abella González <miguel.abella@miguelsoft.net>
2020-06-20 08:30:43 +0200

    Implemented and validated datamining/covariance benchmark.
    Signed-off-by: Miguel Ángel Abella González <miguel.abella@miguelsoft.net>

--- benchmarks/datamining/covariance/covariance.py
+++ benchmarks/datamining/covariance/covariance.py
@@ -15,36 +15,82 @@
 """<replace_with_module_description>"""
 
 from benchmarks.polybench import PolyBench
-from benchmarks.polybench import DatasetSize
+from benchmarks.polybench import PolyBenchParameters
 
 
 class Covariance(PolyBench):
 
-    def __init__(self, dataset_size: DatasetSize = DatasetSize.LARGE):
-        if not isinstance(dataset_size, DatasetSize):
-            raise AssertionError(f'Invalid parameter "dataset_size": "{dataset_size}"')
+    def __init__(self, options: dict, parameters: PolyBenchParameters):
+        super().__init__(options)
 
-        values = {
-            DatasetSize.MINI:           {'M': 28,   'N': 32},
-            DatasetSize.SMALL:          {'M': 80,   'N': 100},
-            DatasetSize.MEDIUM:         {'M': 240,  'N': 260},
-            DatasetSize.LARGE:          {'M': 1200, 'N': 1400},
-            DatasetSize.EXTRA_LARGE:    {'M': 2600, 'N': 3000}
-        }
-        parameters = values.get(dataset_size)
-        if not isinstance(parameters, dict):
-            # Could not find a valid dataset size
-            raise NotImplementedError(f'Dataset size "{dataset_size.name}" not implemented.')
+        # Validate inputs
+        if not isinstance(parameters, PolyBenchParameters):
+            raise AssertionError(f'Invalid parameter "parameters": "{parameters}"')
+
+        # The parameters hold the necessary information obtained from "polybench.spec" file
+        params = parameters.DataSets.get(self.DATASET_SIZE)
+        if not isinstance(params, dict):
+            raise NotImplementedError(f'Dataset size "{self.DATASET_SIZE.name}" not implemented '
+                                      f'for {parameters.Category}/{parameters.Name}.')
+
+        # Adjust the print modifier according to the data type
+        self.set_print_modifier(parameters.DataType)
 
         # Set up problem size
-        self.M = parameters.get('M')
-        self.N = parameters.get('N')
+        self.M = params.get('M')
+        self.N = params.get('N')
 
-    def initialize_array(self, array: list):
-        pass
+    def initialize_array(self, data: list):
+        for i in range(0, self.N):
+            for j in range(0, self.M):
+                data[i][j] = self.DATA_TYPE(i * j) / self.M
 
-    def print_array_custom(self, array: list):
-        self.print_message('\n<array printing>')
+    def print_array_custom(self, cov: list):
+        for i in range(0, self.M):
+            for j in range(0, self.M):
+                if (i * self.M + j) % 20 == 0:
+                    self.print_message('\n')
+                self.print_value(cov[i][j])
+
+    def kernel(self, float_n: float, data: list, cov: list, mean: list):
+# scop begin
+        for j in range(0, self.M):
+            mean[j] = 0.0
+            for i in range(0, self.N):
+                mean[j] += data[i][j]
+            mean[j] /= float_n
+
+        for i in range(0, self.N):
+            for j in range(0, self.M):
+                data[i][j] -= mean[j]
+
+        for i in range(0, self.M):
+            for j in range(0, self.M):
+                cov[i][j] = 0.0
+                for k in range(0, self.N):
+                    cov[i][j] += data[k][i] * data[k][j]
+                cov[i][j] /= float_n - 1.0
+                cov[j][i] = cov[i][j]
+# scop end
 
     def run_benchmark(self):
-        print('Running covariance benchmark')
+        # Array creation
+        float_n = float(self.N)  # we will need a floating point version of N
+        data = self.create_array(2, [self.N, self.M])
+        cov = self.create_array(2, [self.M, self.M])
+        mean = self.create_array(1, [self.M])
+
+        # Initialize array(s)
+        self.initialize_array(data)
+
+        # Start instruments
+        self.start_instruments()
+
+        # Run kernel
+        self.kernel(float_n, data, cov, mean)
+
+        # Stop and print instruments
+        self.stop_instruments()
+
+        # Return printable data as a list of tuples ('name', value)
+        return [('cov', cov)]
