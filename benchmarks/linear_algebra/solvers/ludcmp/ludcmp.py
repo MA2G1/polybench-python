2e41eb4278fdbdc40fdee519036766b5d6311927
Miguel Ángel Abella González <miguel.abella@miguelsoft.net>
2020-06-21 05:07:23 +0200

    Implemented and validated linear-algebra/solvers/ludcmp
    Signed-off-by: Miguel Ángel Abella González <miguel.abella@miguelsoft.net>

---
+++ benchmarks/linear_algebra/solvers/ludcmp/ludcmp.py
@@ -0,0 +1,135 @@
+# Copyright 2019 Miguel Angel Abella Gonzalez <miguel.abella@udc.es>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""<replace_with_module_description>"""
+
+from benchmarks.polybench import PolyBench, PolyBenchParameters
+
+
+class Ludcmp(PolyBench):
+
+    def __init__(self, options: dict, parameters: PolyBenchParameters):
+        super().__init__(options)
+
+        # Validate inputs
+        if not isinstance(parameters, PolyBenchParameters):
+            raise AssertionError(f'Invalid parameter "parameters": "{parameters}"')
+
+        # The parameters hold the necessary information obtained from "polybench.spec" file
+        params = parameters.DataSets.get(self.DATASET_SIZE)
+        if not isinstance(params, dict):
+            raise NotImplementedError(f'Dataset size "{self.DATASET_SIZE.name}" not implemented '
+                                      f'for {parameters.Category}/{parameters.Name}.')
+
+        # Adjust the data type and print modifier according to the data type
+        self.DATA_TYPE = parameters.DataType
+        self.set_print_modifier(parameters.DataType)
+
+        # Set up problem size from the given parameters (adapt this part with appropriate parameters)
+        self.N = params.get('N')
+
+    def initialize_array(self, A: list, b: list, x: list, y: list):
+        fn = self.DATA_TYPE(self.N)
+
+        for i in range(0, self.N):
+            x[i] = self.DATA_TYPE(0)
+            y[i] = self.DATA_TYPE(0)
+            b[i] = (i + 1) / fn / 2.0 + 4
+
+        for i in range(0, self.N):
+            for j in range(0, i + 1):
+                A[i][j] = -self.DATA_TYPE(j % self.N) / self.N + 1
+
+            for j in range(i + 1, self.N):
+                A[i][j] = self.DATA_TYPE(0)
+            A[i][i] = self.DATA_TYPE(1)
+
+        # Make the matrix positive semi-definite.
+        # not necessary for LU, but using same code as cholesky
+        B = self.create_array(2, [self.N], self.DATA_TYPE(0))
+
+        for t in range(0, self.N):
+            for r in range(0, self.N):
+                for s in range(0, self.N):
+                    B[r][s] += A[r][t] * A[s][t]
+
+        for r in range(0, self.N):
+            for s in range(0, self.N):
+                A[r][s] = B[r][s]
+
+    def print_array_custom(self, x: list, name: str):
+        for i in range(0, self.N):
+            if i % 20 == 0:
+                self.print_message('\n')
+            self.print_value(x[i])
+
+    def kernel(self, A: list, b: list, x: list, y: list):
+# scop begin
+        for i in range(0, self.N):
+            for j in range(0, i):
+                w = A[i][j]
+                for k in range(0, j):
+                    w -= A[i][k] * A[k][j]
+                A[i][j] = w / A[j][j];
+
+            for j in range(i, self.N):
+                w = A[i][j]
+                for k in range(0, i):
+                    w -= A[i][k] * A[k][j]
+                A[i][j] = w
+
+        for i in range(0, self.N):
+            w = b[i]
+            for j in range(0, i):
+                w -= A[i][j] * y[j]
+            y[i] = w
+
+        for i in range(self.N - 1, -1, -1):
+            w = y[i]
+            for j in range(i + 1, self.N):
+                w -= A[i][j] * x[j]
+            x[i] = w / A[i][i]
+# scop end
+
+    def run_benchmark(self):
+        # Create data structures (arrays, auxiliary variables, etc.)
+        A = self.create_array(2, [self.N, self.N], self.DATA_TYPE(0))
+        b = self.create_array(1, [self.N], self.DATA_TYPE(0))
+        x = self.create_array(1, [self.N], self.DATA_TYPE(0))
+        y = self.create_array(1, [self.N], self.DATA_TYPE(0))
+
+        # Initialize data structures
+        self.initialize_array(A, b, x, y)
+
+        # Start instruments
+        self.start_instruments()
+
+        # Run kernel
+        self.kernel(A, b, x, y)
+
+        # Stop and print instruments
+        self.stop_instruments()
+
+        # Return printable data as a list of tuples ('name', value).
+        # Each tuple element must have the following format:
+        #   (A: str, B: matrix)
+        #     - A: a representative name for the data (this string will be printed out)
+        #     - B: the actual data structure holding the computed result
+        #
+        # The syntax for the return statement would then be:
+        #   - For single data structure results:
+        #     return [('data_name', data)]
+        #   - For multiple data structure results:
+        #     return [('matrix1', m1), ('matrix2', m2), ... ]
+        return [('x', x)]
